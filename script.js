5fd2bc42b6e159b2d1776a0ed7d9ecaf14b4896a
Eghe Igbinoba <129503571+Eghe001@users.noreply.github.com>
2023-04-06 08:28:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const change =document.getElementById ('image1'); 
+
+change.addEventListener ('click'),(event)=> {
+event.target.style.background ='green';
+event.target.style.color="red";
+} ;
+
+const change2 =document.getElementById ('image2')
+
+change2.addEventListener ('click'), (event) => {
+    event.target.style.background="yellow"
+    event.target.style.color='black';
+};
+
+const change3 =document.getElementById ('image3')
+
+change3.addEventListener ('click'), (event) => {
+    event.target.style.background="Green"
+    event.target.style.color='purple';
+};
+
+const change4 =document.getElementById ('image4')
+
+change4.addEventListener ('click'), (event) => {
+    event.target.style.background="white"
+    event.target.style.color='purple';
+};
+
+const change5 =document.getElementById ('image5')
+
+change5.addEventListener ('click'), (event) => {
+    event.target.style.background="white"
+    event.target.style.color='purple';
+};
+
+const change6 =document.getElementById ('image6')
+
+change6.addEventListener ('click'), (event) => {
+    event.target.style.background="white"
+    event.target.style.color='purple';
+};
